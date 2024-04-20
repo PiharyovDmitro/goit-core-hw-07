2edc26f749bbe16ecf8d21f83f474ceee0370bb4
PiharyovDmitro <dimonm99@gmail.com>
2024-04-20 17:45:01 +0300

    5

--- Bot_Helper2.py
+++ Bot_Helper2.py
@@ -58,6 +58,7 @@ class Record:
         return f"Contact name : {self.name.value}, phones: {';'.join(p.value for p in self.phones)}"
     
 class AddressBook(UserDict):
+    
     def add_record(self, record):
         self.data[record.name.value] = record
 
